591568771674448481a33dc6ab9941643106b098
Artii15 <artur.kowalski15@gmail.com>
2016-05-20 09:54:06 +0200

    Preparing datastructures for holding files descriptors

--- descriptors.c
+++ descriptors.c
@@ -1,7 +1,30 @@
 #include "descriptors.h"
 
+#define MAX_FILENAME_LEN 255
+#define DESCRIPTORS_POOL_SIZE_INCREMENT_VALUE 100
+
+struct FileDescriptor {
+	char fileName[MAX_FILENAME_LEN];
+	int flags;
+	mode_t mode;
+};
+
+struct DescriptorIdx {
+	int idx;
+	struct DescriptorIdx* next;
+};
+
+static struct FileDescriptor* descriptorsPool = 0;
+static int currentPoolSize = 0;
 static int nextDescriptorIdx = 0;
+static struct DescriptorIdx* freedDescriptorsIds = 0;
+
+int reserveNexDescriptorId();
 
 int open(const char *pathname, int flags, mode_t mode) {
-	return 1;
+	return reserveNexDescriptorId();
+}
+
+int reserveNexDescriptorId() {
+	return 0;
 }
